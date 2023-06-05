b266444361c6a556e4c07c8cc568ce8a0ec3c27f
upikoth <upikoth@gmail.com>
2023-06-05 23:53:23 +0300

    fix: разрешил использовать все заголовки и все методы

--- internal/app/apiserver/router.go
+++ internal/app/apiserver/router.go
@@ -79,6 +79,8 @@ func (s *ApiServer) initRoutes() {
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "*")
 
 		if c.Request.Method == http.MethodOptions {
 			c.AbortWithStatus(http.StatusNoContent)
